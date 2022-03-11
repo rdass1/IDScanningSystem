b0938c8340260a0ac68ddef9344b5067bbacef9a
rdass1 <rdass1@hawk.iit.edu>
2022-03-11 13:48:58 -0600

    first commit

--- program.py
+++ program.py
@@ -8,16 +8,23 @@ from serial.tools import list_ports
 
 
 
+scanner = {
+    "location": "front-desk",
+    "building": "mainBuilding",
+    "VID": "0525",
+    "PID": "A4A7"
+}
+
+
+
 #open database
-try:
-    conn = psycopg2.connect(
-        host = "104.194.100.20",
-        port = "25566",
-        database = "id_database_system",
-        user = "access01",
-        password = "access01")
-except:
-    print("database can't connect")
+conn = psycopg2.connect(
+    host = "104.194.100.20",
+    port = "25566",
+    database = "id_database_system",
+    user = "access01",
+    password = "access01")
+
 
 cur = conn.cursor()
 
@@ -31,37 +38,49 @@ cur = conn.cursor()
 
 
 
-VID = "0525"
-PID = "A4A7"
+
 
 device_list = list_ports.comports()
 for device in device_list:
     if (device.vid != None or device.pid != None):
-        if ('{:04X}'.format(device.vid) == VID and
-            '{:04X}'.format(device.pid) == PID):
+        if ('{:04X}'.format(device.vid) == scanner["VID"]and
+            '{:04X}'.format(device.pid) == scanner["PID"]):
             port = device.device
             break
         port = None
-
 ser = serial.Serial(port, 19200, timeout = 0)
 while True:
+    
     try:
         line = ser.readline().decode()
-        print(line)
         if len(line) > 0 and line.startswith("AB",0,2) and line[2:].isdigit() and len(line[2:]) == 10:
-            
+            # print(line)
             cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
             command = "SELECT * FROM memberInfo.member WHERE id = '%s';" % (line)
             #print(command)
             cur.execute(command)
             #print(cur.rowcount)
+            currentUser = cur.fetchone()
+            command = "SELECT * FROM memberInfo.activeMember WHERE uid = '%s';" % (currentUser['uid'])
+            cur.execute(command)
             output = cur.fetchone()
             if (output != None):
-                print(output)
-                print(output['uid'])
-            
+                if(output['active']==False):
+                    command1 = "UPDATE memberInfo.activeMember SET active = TRUE WHERE uid = '%s';" % (output['uid'])
+                else:
+                    command1 = "UPDATE memberInfo.activeMember SET active = FALSE WHERE uid = '%s';" % (output['uid'])
+                # print(output)
+                # print(output['uid'])
+                # print(command1)
+                cur.execute(command1)
+                conn.commit()
+            else:
+                
+                cur.execute("""INSERT INTO memberInfo.activeMember (uid,active,building) VALUES (%s,%s,%s);""",((currentUser['uid']),("True"),(scanner["building"])))
+                conn.commit()
             cur.close()
-    except(exception):
-        print(exception)
+    except Exception as e:
+        print(e)
         pass
+
 
