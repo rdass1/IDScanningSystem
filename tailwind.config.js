10ae889f7672de170c3e30dc32186ff189e2021f
Rishon Dass <rdass1@hawk.iit.edu>
2022-03-31 18:23:11 -0500

    added tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./views/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'nav-blue' : "#111827",
+        'table-gray': '#D9D9D9'
+      },
+    },
   },
   plugins: [],
 }
