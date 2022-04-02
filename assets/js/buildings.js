a06649ab6d3beb84b8663560afc4ac0f130a5d22
Rishon Dass <rdass1@hawk.iit.edu>
2022-04-02 15:59:07 -0500

    revert

--- assets/js/buildings.js
+++ assets/js/buildings.js
@@ -38,4 +38,14 @@ const showNav = (flag) => {
     }
 }
 
+const showNav2 = (flag) => {
+    if(flag) {
+        nav.classList.toggle("translate-y-0");
+        open.classList.toggle("hidden");
+        close.classList.toggle("hidden");
+        //main.classList.toggle("-translate-y-72");
+        //nav.classList.toggle("translate-y-0");
+    }
+}
+
 
