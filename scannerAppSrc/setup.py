acfb1edd59e03824d5c8e1051918f2ec6119d91d
Rishon Dass <rdass1@hawk.iit.edu>
2022-06-07 05:43:18 -0500

    updated files

---
+++ scannerAppSrc/setup.py
@@ -0,0 +1,17 @@
+from ssl import Options
+from setuptools import setup
+
+DATA_FILES = ['startScreen.ui','programRunningScreen.ui',"configScanner.ui","configDatabase.ui","configBuilding.ui"]
+
+OPTIONS = {
+    'iconfile':'icon.icns',
+    'argv_emulation': True,
+    'packages' : ['certifi']
+}
+setup(
+    app=["main.py"],
+    name="AnB Scanner",
+    data_files = DATA_FILES,
+    options={'p2app': OPTIONS},
+    setup_requires=["py2app"],
+)
